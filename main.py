88f933d75b4d10d67fbe55b026167c19ace441c9
Samuel Diniz Tenório <samueldiniztenorio.tech@gmail.com>
2026-09-03 10:26:06 -0300

    feat: implement core s3 builder initialization and main entrypoint

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+from dotenv import load_dotenv
+import logging
+from s3_builder import S3Builder
+
+load_dotenv()
+
+AWS_ENDPOINT_URL=os.getenv('AWS_ENDPOINT_URL')
+AWS_ACCESS_KEY_ID=os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY=os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_REGION=os.getenv('AWS_REGION')
+
+"""Basic log configuration"""
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+logger = logging.getLogger(__name__)
+
+def main():
+    s3_builder = S3Builder(
+        endpoint_url=AWS_ENDPOINT_URL,
+        access_key_id=AWS_ACCESS_KEY_ID,
+        secret_access_key=AWS_SECRET_ACCESS_KEY,
+        region=AWS_REGION
+    )
+
+if __name__ == '__main__':
+    main()
---
+++ s3_builder.py
@@ -0,0 +1,32 @@
+import logging
+import boto3
+from mypy_boto3_s3 import S3Client
+from botocore.exceptions import ClientError
+
+logger = logging.getLogger('main')
+
+class S3Builder:
+    """Builder utility for provisioning AWS S3."""
+    
+    def __init__(
+        self,
+        endpoint_url: str = 'http://localhost:4566',
+        access_key_id: str = 'test',
+        secret_access_key: str = 'test',
+        region: str = 'us-east-1'
+    ) -> None:
+        try:
+            self.s3: S3Client = boto3.client(
+                    service_name='s3',
+                    endpoint_url=endpoint_url,
+                    aws_access_key_id=access_key_id,
+                    aws_secret_access_key=secret_access_key,
+                    region_name=region 
+                )
+        except ClientError as ex:
+            logger.warning('Failed in create S3 client: %s', ex)
+            raise
+        except Exception as ex:
+            logger.warning('Unexpected error while initializing S3Builder: %s', ex)
+            raise
+        
