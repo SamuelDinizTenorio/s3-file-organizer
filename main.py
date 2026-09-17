09ca048e75d6d8c77af28f0b92ba163c616741b3
Samuel Diniz Tenório <samueldiniztenorio.tech@gmail.com>
2026-09-17 17:22:16 -0300

    refactor(main): add type hints and environment variable fallbacks

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ from s3_builder import S3Builder
 
 load_dotenv()
 
-AWS_ENDPOINT_URL = os.getenv("AWS_ENDPOINT_URL")
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-AWS_REGION = os.getenv("AWS_REGION")
+AWS_ENDPOINT_URL = os.getenv("AWS_ENDPOINT_URL", "http://localhost:4566")
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID", "test")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY", "test")
+AWS_REGION = os.getenv("AWS_REGION", "us-east-1")
 
 """Basic log configuration"""
 logging.basicConfig(
@@ -19,7 +19,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def main():
+def main() -> None:
     s3_builder = S3Builder(
         endpoint_url=AWS_ENDPOINT_URL,
         access_key_id=AWS_ACCESS_KEY_ID,
